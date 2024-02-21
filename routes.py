016a41bc4206cc35b1dabbe5529b7120a13ad5d1
Alvatron-2 <bigamoorefuller@gmail.com>
2024-02-21 13:48:46 +0000

    5

--- routes.py
+++ routes.py
@@ -9,8 +9,12 @@ from __main__ import app
 def floodings(methods=["POST","GET"]):
     response = get("https://environment.data.gov.uk/flood-monitoring/id/floods")
     data = Payload(response.content)
-    
-    print(str(data.meta))
-    print("Publisher:",data.meta["publisher"])
+    #this is an object storing all of the API readings :)
 
-    return render_template('Floodings.html')
+    location_names = []
+    for location in data.items:
+        location_names.append(location["eaAreaName"])
+
+    
+
+    return render_template('Floodings.html',location_names=location_names)
