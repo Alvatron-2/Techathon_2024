3ae6a7eb1ccc449682e331eb02fd55c25b262664
Alvatron-2 <bigamoorefuller@gmail.com>
2024-02-21 14:59:16 +0000

    9

--- routes.py
+++ routes.py
@@ -5,7 +5,8 @@ from Models import Payload,flood_reading
 
 from __main__ import app
 
-@app.route('/')
+
+@app.route('/floodings')
 def floodings(methods=["POST","GET"]):
     response = get("https://environment.data.gov.uk/flood-monitoring/id/floods")
     data = Payload(response.content)
@@ -16,6 +17,11 @@ def floodings(methods=["POST","GET"]):
         readings.append(flood_reading(location["eaAreaName"],location["severity"],
                                       location["message"]))
 
+    locations_unique = []
+    for location in data.items:
+        if location["eaAreaName"] not in locations_unique:
+            locations_unique.append(location["eaAreaName"])
     
 
-    return render_template('Floodings.html',readings=readings)
+    return render_template('Floodings.html',readings=readings,
+                           locations_unique=locations_unique)
