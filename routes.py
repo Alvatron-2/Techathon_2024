bc25758a76d795e8c0037d968c9d269cba4f9cbe
Alvatron-2 <bigamoorefuller@gmail.com>
2024-02-21 13:15:39 +0000

    3

--- routes.py
+++ routes.py
@@ -1,7 +1,16 @@
 from flask import render_template
+from requests import get
+
+from Models import Payload
 
 from __main__ import app
 
 @app.route('/')
-def hello():
-    return render_template('Base.html')
+def floodings(methods=["POST","GET"]):
+    response = get("https://environment.data.gov.uk/flood-monitoring/id/floods")
+    data = Payload(response.content)
+    
+    print(str(data.meta))
+    print("Publisher:",data.meta["publisher"])
+
+    return render_template('Floodings.html')
